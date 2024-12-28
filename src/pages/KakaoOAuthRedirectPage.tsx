d4dd84c83b5d803ab0a45f042a8d7e9bd8e27ec1
JeongHoon Woo <frndhoon@gmail.com>
2024-12-28 21:50:00 +0900

    Revert "🐛 Kakao OAuth login error 수정"

--- src/pages/KakaoOAuthRedirectPage.tsx
+++ src/pages/KakaoOAuthRedirectPage.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ky from 'ky';
 
-const BACKEND_API_URL = import.meta.env.VITE_BACKEND_API_URL;
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const KakaoOAuthRedirectPage = () => {
   const navigate = useNavigate();
@@ -14,16 +14,18 @@ export const KakaoOAuthRedirectPage = () => {
       if (code) {
         try {
           await ky
-            .get(`${BACKEND_API_URL}/login/kakao`, {
+            .get(`${BACKEND_URL}/login/kakao`, {
               searchParams: { code },
-              credentials: 'include',
             })
-            .then(() => {
-              navigate('/');
+            .then((res) => {
+              const token = res.headers.get('jwt');
+              if (token) {
+                localStorage.setItem('token', token);
+                navigate('/');
+              }
             });
         } catch (error) {
           console.error(error);
-          navigate('/login');
         }
       }
     };
